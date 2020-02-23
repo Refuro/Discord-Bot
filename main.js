0ce279200717945fe82f281a25f06bf90b61ff7e
Refuro <61066621+Refuro@users.noreply.github.com>
2020-02-23 03:28:08 -0600

    Updated main.js
    main.js was updated to test out the on functionality in discord.js

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
-
-client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
-
-client.login('token');
+client.once('ready', () => {
+	console.log('Ready!');
+});
+client.on('message', message => {
+	console.log(message.content);
+});
+client.login('NjgwOTkyMjQ1MDEyNDk2Mzkw.XlH-XA.gQuGHClNXJQsziA6Z4Qnmkhf6V0');
