eb9347dc61c8514a867703000caa12cb4967461f
Refuro <61066621+Refuro@users.noreply.github.com>
2020-02-23 05:06:09 -0600

    Added !time, !help, !hello, !channel
    Added the below commands and functionality. *** beside a name means it may be removed in the future.
    
    * !time returns the time. I need to add better formatting later
    * !help returns a list of commands and general help, I need to format this probably better as well.
    * !hello returns a simple greeting to test the .author property, ***
    * !channel returns the name of the chat channel you are in. Again just to test the ,channel property. ***

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,6 +6,22 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 client.on('message', message => {
-	console.log(message.content);
+	if (message.content === '!help'){
+        message.channel.send('Currently this bot has little functionality and is in testing phase.\n\nCurrent Commands: !hello(returns a greeting).\n!time(returns time).\n\nFuture commands: !(stock ticker name). For example, !MSFT would return \'Microsoft:$178.72\'');
+        console.log('!help was called');
+        }
+    if (message.content === '!hello'){
+        message.channel.send('Hey there @'+String(message.author));
+        console.log('!hello was called');
+        }
+    if (message.content === '!time'){
+        const now = new Date();
+        message.channel.send(String(now.getHours()) + ":" + String(now.getMinutes()) + ":" + String(now.getSeconds()));
+        console.log('!time was called');
+        }
+    if (message.content === '!channel'){
+        message.channel.send(String(message.channel));
+        console.log('!channel was called');
+    }
 });
-client.login('NjgwOTkyMjQ1MDEyNDk2Mzkw.XlH-XA.gQuGHClNXJQsziA6Z4Qnmkhf6V0');
+client.login(token);
